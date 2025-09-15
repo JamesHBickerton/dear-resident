6905dfe59a3b1c6f7179ce789f107a04d1d6af64
JamesHBickerton <jameshuiabickerton@gmail.com>
2025-09-15 20:17:11 +0100

    Updated hero and features sections; added new images and CSS styling

--- dearresident/src/App.js
+++ dearresident/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import heroImg from "./img/heroimg.png";
+import heroImg from "./img/heroimg.jpg";
+import logoImg from "./img/drLogo.png";
 
 function App() {
   return (
@@ -16,13 +17,15 @@ function App() {
 
 function HeroSection() {
   return (
-    <section
-      className="hero"
-      style={{ backgroundImage: `url(${heroImg})` }} // React will resolve the path
-    >
+    <section className="hero" style={{ backgroundImage: `url(${heroImg})` }}>
       <div className="hero-content">
-        <h1 className="main-header">DearResident</h1>
-        <h2 className="hero-subtitle">Connecting Families, Enhancing Care</h2>
+        <div className="hero-branding">
+          <img src={logoImg} alt="DearResident-Logo" className="hero-logo" />
+          <h1 className="main-header">DearResident</h1>
+          <h2 className="hero-subtitle">
+            Connecting Families, Supporting Care
+          </h2>
+        </div>
         <button>Learn More</button>
       </div>
     </section>
@@ -42,10 +45,41 @@ function Header() {
 }
 
 function FeatureHighlights() {
+  const features = [
+    {
+      title: "Resident Profiles",
+      description: "View care notes and receive daily updates.",
+    },
+    {
+      title: "Family Logs",
+      description: "Leave messages and notes for your loved ones.",
+    },
+    {
+      title: "Mobility Tracker",
+      description: "Track physical activity and mobility progress.",
+    },
+    {
+      title: "Visit Scheduler",
+      description: "Schedule and receive reminders for family visits.",
+    },
+  ];
+
   return (
     <section className="features">
-      <h3>Our Features</h3>
-      <div className="feature-cards">{/* Map through features */}</div>
+      <h3 className="features-header">Our Features</h3>
+      <p>
+        <span>DearResident</span> makes it easier to connect with your loved
+        ones, supporting their activity and well-being, and helping you stay
+        close and involved, even from a distance.
+      </p>
+      <div className="feature-cards">
+        {features.map((feature, index) => (
+          <div className="feature-card" key={index}>
+            <h4>{feature.title}</h4>
+            <p>{feature.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
